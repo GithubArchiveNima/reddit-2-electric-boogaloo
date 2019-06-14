b9ce8f2b1f2a2b7278a7e559e6658d65234ada2b
Nima Boscarino <nboscari@sfu.ca>
2019-06-14 10:18:53 -0700

    show the topic name for conversations list

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,4 +1,11 @@
 class ConversationsController < ApplicationController
   def index
+    topic_id = params['topic_id']
+    # req.params.topic_id
+    @topic = Topic.find(topic_id)
+
+    # @conversations = Conversation.find_by(topic: topic)
+
   end
+
 end
